61f0f8a2c39a556d93b2efde99a01a77a4d75488
Jared Kjar <jaredkjar@gmail.com>
2018-01-04 14:28:45 -0800

    added progress button

--- controllers/Login.js
+++ controllers/Login.js
@@ -1,16 +1,25 @@
 angular.module("Quiz App")
     .controller("Login", Login);
 
-function Login(){
-    
+Login.$inject = ["LoginService"];
+
+function Login(ls){
     this.goRegister = () => {
         window.location.replace("../register/register.html");
     }
-
     this.userLogin = () => {
         const profile = {
-            userName: this.email,
+            email: this.email,
             password: this.password
-        }
+        };
+        const promise = ls.post(profile)
+        promise.then(
+            response => {
+                this.fade = false;
+            },
+            err => {
+                this.fade = false;
+                console.log(err);
+            });
     }
 }
