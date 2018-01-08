41d34da9bb1015562b8c043f19cdf33a8c6ed5e1
Jared Kjar <jaredkjar@gmail.com>
2018-01-08 11:51:47 -0800

    before implementing UI Router

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-(function(){angular.module('Quiz App', [])})();
+(function(){
+    angular
+    .module('Quiz App', [])
+    // .config(function ($stateProvider, $urlRouterProvider) {
+    //     $urlRouterProvider.otherwise('/home');
+
+    //     $stateProvider 
+    //         .state('home'), {
+    //             url: '/home',
+    //             templateUrl: 'home/home.html'
+    //         }
+
+    //         .state('login'), {
+    //             url: 'login',
+    //             templateUrl: 'login/login.html'
+    //         }
+    // })
+
+
+})();
